2f8441a5dcfe508bdaa3970a540e42a5d5a5d5c2
Sonika <sonikapoudyal5@gmail.com>
2021-06-14 18:41:22 -0400

    node asynchronicity

--- src/page-template.js
+++ src/page-template.js
@@ -90,7 +90,7 @@ module.exports = templateData => {
     ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
     </footer>
   </body>
   </html>
